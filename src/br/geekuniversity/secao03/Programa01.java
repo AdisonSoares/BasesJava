017a5502bc1747a32adf4448ac2b8b479d09a1ae
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-05-30 12:22:48 -0300

    Secao03 - Variaveis 01

--- src/br/geekuniversity/secao03/Programa01.java
+++ src/br/geekuniversity/secao03/Programa01.java
@@ -1,22 +1,19 @@
-/*DocumentaÁ„o por Javadoc:*/
+/*Documenta√ß√£o por Javadoc:*/
 /**
- * Programa b·sico saÌda de dados e entrada de dados
- * Vari·veis
- * OBS - Todo o nome de classe em Java comeÁa com letra maiuscula, n„o se pode iniciar
+ * Programa b√°sico sa√≠da de dados e entrada de dados
+ * Vari√°veis
+ * OBS - Todo o nome de classe em Java come√ßa com letra maiuscula, n√£o se pode iniciar
  * uma classe com numero ou caracter especial e caso se tenha nome composto ele precisa
  * ser junto ou com underline e a cada inicio de palavra colocar letra maiuscula.
  * Exemplo:
  * GuardaChuva ou Guarda_Chuva
- *  
- * @author Positivo
- *
  */
 package br.geekuniversity.secao03;
 public class Programa01 {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		System.out.println("Ol· meu irm„o");
-		//DeclaraÁ„o de Variates
+		System.out.println("Ol√° meu irm√£o");
+		//Declara√ß√£o de Variates
 		int valor;
 		int num1,num2,num3;
 		char caractere;
@@ -26,5 +23,6 @@ public class Programa01 {
 		int num5=4,num6=7,num9=10,num10=99;
 		float _valor=34.2f;
 		double _valor2=34.2;
+		
 	}
 }
