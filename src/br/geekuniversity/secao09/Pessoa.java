9854386e81f3ae647bd7f8adac120dbfa6ff0df2
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-05-30 22:39:46 -0300

    Secao09 - Classes, Atributos e Metodos 01

--- src/br/geekuniversity/secao09/Pessoa.java
+++ src/br/geekuniversity/secao09/Pessoa.java
@@ -1,20 +1,18 @@
-/*DocumentaÁ„o por Javadoc:*/
+/*Documenta√ß√£o por Javadoc:*/
 /**
- * Classes, Atributos e MÈtodos
- * @author Positivo
- *
+ * Classes, Atributos e M√©todos
  */
 
-/*Atributos s„o caracteristicas da classe/modelo/modelo de dados.
-  Podemos entender atributos como vari·veis da classe.
-  Outra forma de nomear os atributos È chamando-os de estados.
-  Atributos s„o nomeados em letras min˙sculas, sem espaÁo, sem 
-  caracteres especiais, sem acentuaÁ„o.
-  O nome de uma vari·vel pode ser iniciada apenas com letras e underline.
-  Podemos entender os mÈtodos como a aÁ„o que È realizada por um objeto da classe.
-  Os mÈtodos s„o comportamentos dos objetos da classe.
-  Os par‚metros s„o os atributos discriminados na classe.
-  O this fala do prÛprio objeto da classe.*/
+/*Atributos s√£o caracteristicas da classe/modelo/modelo de dados.
+  Podemos entender atributos como vari√°veis da classe.
+  Outra forma de nomear os atributos √© chamando-os de estados.
+  Atributos s√£o nomeados em letras min√∫sculas, sem espa√ßo, sem 
+  caracteres especiais, sem acentua√ß√£o.
+  O nome de uma vari√°vel pode ser iniciada apenas com letras e underline.
+  Podemos entender os m√©todos como a a√ß√£o que √© realizada por um objeto da classe.
+  Os m√©todos s√£o comportamentos dos objetos da classe.
+  Os par√¢metros s√£o os atributos discriminados na classe.
+  O this fala do pr√≥prio objeto da classe.*/
 
 package br.geekuniversity.secao09;
 public class Pessoa {
@@ -22,23 +20,21 @@ public class Pessoa {
 	String nome,email;
 	int ano_nascimento;
 	
-	//Construtor padr„o/vazio
+	//Construtor padr√£o/vazio
 	public Pessoa() {}
 	
-	//Construtor com par‚metros
+	//Construtor com par√¢metros
 	public Pessoa(String nome, String email, int ano_nascimento) {
 		this.nome = nome;
 		this.email = email;
 		this.ano_nascimento = ano_nascimento;
 	}
-	/**
-	 * MÈtodo exibiÁ„o
-	 */
+	
+	 //M√©todo exibi√ß√£o
 	void imprimeInformacoes() {
 		System.out.println(this.nome);
 		System.out.println(this.email);
 		System.out.println(this.ano_nascimento);
 		System.out.println();
 	}
-	
 }
