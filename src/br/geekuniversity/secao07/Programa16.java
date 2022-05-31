fb7789f0471d6417bb41ec32d4120e4bf267924a
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-05-30 21:25:12 -0300

    Secao07 - Vetor 01

--- src/br/geekuniversity/secao07/Programa16.java
+++ src/br/geekuniversity/secao07/Programa16.java
@@ -1,4 +1,4 @@
-/*Documentação por Javadoc:*/
+/*DocumentaÃ§Ã£o por Javadoc:*/
 /**
  * Vetores parte 1
  * Arrays ou vetores unidimensionais
@@ -8,30 +8,25 @@
  * vetor[2]=5;//Terceiro elemento
  * vetor[3]=7;//Quarto elemento
  * vetor[4]=9;//Quinto elemento
- * vetor[indice];*OBS-Para calcular o último elemento de um vetor se usa(n-1),
- * sendo n º de elementos*Exemplo 5-1=4(última posição do vetor)
- * @author Positivo
- *
+ * vetor[indice];*OBS-Para calcular o Ãºltimo elemento de um vetor se usa(n-1),
+ * sendo n Âº de elementos*Exemplo 5-1=4(Ãºltima posiÃ§Ã£o do vetor)
  */
-
 package br.geekuniversity.secao07;
 
 public class Programa16 {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		// Declarando um vetor sem especificar o tamanho/posições
+		// Declarando um vetor sem especificar o tamanho/posiÃ§Ãµes
 		int vetor[];
 
-		// Declaração de um vetor com as quantidade de posições
+		// DeclaraÃ§Ã£o de um vetor com as quantidade de posiÃ§Ãµes
 		int numeros[] = new int[5];
 		float valores[] = new float[5];
 		char caracteres[] = new char[20];
 		String nomes[] = new String[4];
 
 		// Declarar e inicializar
-		int outros_numeros[] = { 1, 3, 5, 7, 9 };// Tamanho do vetor é 5, última posição 5-1=4
-		String frutas[] = { "Manga", "Uva", "Banana" };// O vetor é de tamanho 3, última posição 2
-
+		int outros_numeros[] = { 1, 3, 5, 7, 9 };// Tamanho do vetor Ã© 5, Ãºltima posiÃ§Ã£o 5-1=4
+		String frutas[] = { "Manga", "Uva", "Banana" };// O vetor Ã© de tamanho 3, Ãºltima posiÃ§Ã£o 2
 	}
-
 }
