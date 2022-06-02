d620f70f15e6c2d506fd1efb0eebf517602f23e9
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-06-02 15:54:35 -0300

    Secao11 - Modificadores de acesso - Encapsulamento

--- src/br/geekuniversity/secao11/Programa27.java
+++ src/br/geekuniversity/secao11/Programa27.java
@@ -1,10 +1,9 @@
 /*Modificador de acesso private
  * O private torna aquele atributo exclusivo da classe, ou seja, somente
- * a classe pode acessar e modificar ela, n„o se pode importar para outra classe
+ * a classe pode acessar e modificar ela, n√£o se pode importar para outra classe
  * mesmo estando no mesmo projeto.*/
 
 package br.geekuniversity.secao11;
-
 import br.geekuniversity.secao11.Cliente;
 
 @SuppressWarnings("unused")
@@ -14,11 +13,11 @@ public class Programa27 {
 		Cliente cli1 = new Cliente(27, "Creuza Ziranda", "Rua sem nome, sem numero, sem nada");
 		
 		System.out.println("Nome "+cli1.getNome());
-		System.out.println("EndereÁo "+cli1.getEndereco());
+		System.out.println("Endere√ßo "+cli1.getEndereco());
 		
-		//Agora s„o private n„o podem ser acessados dessa forma
+		//Agora s√£o private n√£o podem ser acessados dessa forma
 		//System.out.println("Nome: " + cli1.nome);
-		//System.out.println("EndereÁo: " + cli1.endereco);
+		//System.out.println("Endere√ßo: " + cli1.endereco);
 		//cli1.dizer_oi();
 	}
 
