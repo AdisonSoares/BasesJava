6380ed5d2e7bc2c9be30876c306e55fbee8b66b9
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-06-02 15:54:16 -0300

    Secao11 - Modificadores de acesso - Encapsulamento

--- src/br/geekuniversity/secao11/Programa25.java
+++ src/br/geekuniversity/secao11/Programa25.java
@@ -1,21 +1,21 @@
-/*No exemplo da tentativa de saque alÈm do saldo o programa n„o permite o saque
+/*No exemplo da tentativa de saque al√©m do saldo o programa n√£o permite o saque
  *continuando com o saldo anterior.
- *N„o se pode deixar vari·veis disponÌveis para os clientes È preciso encapsular para
- *n„o se ter problemas com a quebra do cÛdigo ou acesso direto ‡ vari·vel.
- *Para se ter acesso ao mÈtodo ou vari·vel encapsulada com o modificador de acesso
- *private È preciso usar os mÈtodos getters e setters.
- *O mÈtodo Getter È um mÈtodo p˙blico que serve para consultar dados.
- *A nomenclatura desses mÈtodos È public tipoDeVari·vel getnomeDoAtributo().
- *O ideal È que todos os atributos de uma classe sejam privados.*/
-package br.geekuniversity.secao11;
+ *N√£o se pode deixar vari√°veis dispon√≠veis para os clientes √© preciso encapsular para
+ *n√£o se ter problemas com a quebra do c√≥digo ou acesso direto √† vari√°vel.
+ *Para se ter acesso ao m√©todo ou vari√°vel encapsulada com o modificador de acesso
+ *private √© preciso usar os m√©todos getters e setters.
+ *O m√©todo Getter √© um m√©todo p√∫blico que serve para consultar dados.
+ *A nomenclatura desses m√©todos √© public tipoDeVari√°vel getnomeDoAtributo().
+ *O ideal √© que todos os atributos de uma classe sejam privados.*/
 
+package br.geekuniversity.secao11;
 import br.geekuniversity.secao11.Cliente;
 import br.geekuniversity.secao11.Conta;
 
 @SuppressWarnings("unused")
 public class Programa25 {
 	public static void main(String[] args) {
-		Cliente cliente_1 = new Cliente(45, "Jo„o de Barro", "Qnl 20, Conj 03, Casa 02");
+		Cliente cliente_1 = new Cliente(45, "Jo√£o de Barro", "Qnl 20, Conj 03, Casa 02");
 		Cliente cliente_2 = new Cliente(14, "Maria Gilda", "Qnj 40, Conj 08, Apt 201");
 
 		Conta conta_1 = new Conta(1, 100.00f, 200.00f, cliente_1);
