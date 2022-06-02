ecf95f8f3325364ed2a3f16f473b97148e240c2b
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-06-02 16:15:42 -0300

    Secao12 - Heranca, polimorfismo, sobrescrita e sobrecarga

--- src/br/geekuniversity/secao12/Programa30.java
+++ src/br/geekuniversity/secao12/Programa30.java
@@ -1,13 +1,11 @@
-//Sobrescrita de método
-/*Também conhecido como overriding, ocorre no ato de sobrescrever(escrever por cima)
-**métodos da classe pai, ou super classe, em classes filhas.*/
+//Sobrescrita de mÃ©todo
+/*TambÃ©m conhecido como overriding, ocorre no ato de sobrescrever(escrever por cima)
+**mÃ©todos da classe pai, ou super classe, em classes filhas.*/
 
 package br.geekuniversity.secao12;
 public class Programa30 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
-
 }
