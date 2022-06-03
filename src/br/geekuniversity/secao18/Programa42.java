7df87c4d4543f68075c57ca0f45bbfb6c150f807
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-06-02 23:07:44 -0300

    Secao18 - Conhecendo a java lang

--- src/br/geekuniversity/secao18/Programa42.java
+++ src/br/geekuniversity/secao18/Programa42.java
@@ -1,13 +1,13 @@
 /*Escrevendo em arquivos 01*/
 
-/*Pacotes/Importações*/
+/*Pacotes/ImportaÃ§Ãµes*/
 package br.geekuniversity.secao18;
 
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-/*Método main*/
+/*MÃ©todo main*/
 public class Programa42 {
 	public static void main(String[] args) {
 
@@ -21,7 +21,7 @@ public class Programa42 {
 			mensagem02.println(mensagem03);
 			mensagem02.close();
 		} catch (FileNotFoundException e) {
-			System.out.println("Não foi possivel criar o arquivo de mensagem");
+			System.out.println("NÃ£o foi possivel criar o arquivo de mensagem");
 		}
 
 		/* Imprimir no console */
@@ -30,6 +30,5 @@ public class Programa42 {
 		System.out.println(mensagem01);
 
 		sc.close();
-		
 	}
 }
