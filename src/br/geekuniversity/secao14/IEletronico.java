66d0dc3b39d27bcd5e2431420e87e74a2834d6c6
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-06-02 22:57:11 -0300

    Secao14 - Interfaces

--- src/br/geekuniversity/secao14/IEletronico.java
+++ src/br/geekuniversity/secao14/IEletronico.java
@@ -1,7 +1,7 @@
-/*Uma interface pode conter constantes e mÈtodos abstratos.
-**Interface È aplicada para servir de contrato para produtos eletronicos.
-**Todo o produto eletronico que implementar esta interface obrigatoriamente
-**dever„o implementar os mÈtodos abstratos.*/
+/*Uma interface pode conter constantes e m√©todos abstratos.
+**Interface √© aplicada para servir de contrato para produtos eletr√¥nicos.
+**Todo o produto eletr√¥nico que implementar esta interface obrigatoriamente
+**dever√£o implementar os m√©todos abstratos.*/
 
 package br.geekuniversity.secao14;
 public interface IEletronico {
