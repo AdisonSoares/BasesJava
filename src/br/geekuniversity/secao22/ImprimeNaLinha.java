367dfe8a8530b2dd05810913e1d027423050c5c2
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-06-03 12:41:36 -0300

    Secao22 - Default methods, classes anon., lambdas, methods. ref., streams, datas

--- src/br/geekuniversity/secao22/ImprimeNaLinha.java
+++ src/br/geekuniversity/secao22/ImprimeNaLinha.java
@@ -1,10 +1,9 @@
 package br.geekuniversity.secao22;
-
 import java.util.function.Consumer;
 
 /**
  * @Nao_serve
- * Depois de implementar no programa 69 a mesma funcao anonima
+ * Depois de implementar no programa 69 a mesma função anonima
  * essa classe nao tem serventia.
  */
 public class ImprimeNaLinha implements Consumer<String> {
