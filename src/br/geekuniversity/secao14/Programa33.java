7d7ac232dc439d8149250261778692b10ed8c964
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-06-02 22:57:41 -0300

    Secao14 - Interfaces

--- src/br/geekuniversity/secao14/Programa33.java
+++ src/br/geekuniversity/secao14/Programa33.java
@@ -1,28 +1,26 @@
 //Interfaces
-/*Interfaces s„o conhecidos como  contratos.
+/*Interfaces s√£o conhecidos como  contratos.
 **Uma empresa criou um contrato "com regras" para definir
-**a implementaÁ„o de um produto/serviÁo.
-**Duas pessoas v„o implementar um produto/serviÁo baseado neste contrato, ou seja,
-**o produto/serviÁo v„o ser iguais,cada um com suas caracteristicas.
-**Quem implementar esse contrato È obrigado a seguir as regras.
-**A diferenÁa entre uma interface e um classe abstrata È que a classe abstrata
-**permite criar vari·veis e mÈtodos com implementaÁ„o assim como constantes e 
-**mÈtodos abstratos mas a interface permite apenas criar constantes e mÈtodos
+**a implementa√ß√£o de um produto/servi√ßo.
+**Duas pessoas v√£o implementar um produto/servi√ßo baseado neste contrato, ou seja,
+**o produto/servi√ßo v√£o ser iguais,cada um com suas caracteristicas.
+**Quem implementar esse contrato √© obrigado a seguir as regras.
+**A diferen√ßa entre uma interface e um classe abstrata √© que a classe abstrata
+**permite criar vari√°veis e m√©todos com implementa√ß√£o assim como constantes e 
+**m√©todos abstratos mas a interface permite apenas criar constantes e m√©todos
 **abstratos, permitindo um controle maior do que as classes filhas
-**estar„o implementando.*/
+**estar√£o implementando.*/
 
 package br.geekuniversity.secao14;
-
 import br.geekuniversity.secao14.Ventilador;
 
 @SuppressWarnings("unused")
 public class Programa33 {
-
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
 		Ventilador ventilador = new Ventilador();
 		
-		System.out.println("A marca do aparelho È: "+ventilador.MARCA);
+		System.out.println("A marca do aparelho √©: "+ventilador.MARCA);
 		ventilador.desligar();
 		ventilador.ligar();
 		ventilador.desligar();
