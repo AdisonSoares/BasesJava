183566008025bcca743cbbb88bf730f6b5c42d3e
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-06-02 23:06:07 -0300

    Secao18 - Conhecendo a java lang

--- src/br/geekuniversity/secao18/Caixa.java
+++ src/br/geekuniversity/secao18/Caixa.java
@@ -1,10 +1,10 @@
-/*Caixa de objetos genêricos*/
+/*Caixa de objetos genÃªricos*/
 
 package br.geekuniversity.secao18;
 public class Caixa {
 	
 	/**
-	 * Esse vetor vai guardar a contas, como um banco de dados da memória ram
+	 * Esse vetor vai guardar a contas, como um banco de dados da memÃ³ria ram
 	 */
 	private Object[] objeto;
 	private int posicaoLivre;
@@ -13,14 +13,14 @@ public class Caixa {
 	 * O instanciador caixa vai inicializar o vetor de armazenamento das contas
 	 */
 	public Caixa() {
-		//Limite até 100 objetos, de 0 até 99
+		//Limite atÃ© 100 objetos, de 0 atÃ© 99
 		objeto = new Object[100];
 		posicaoLivre = 0;
 	}
 	
 	/**
-	 * Método para armazenar os dados da conta e adicionar uma contagem
-	 * para a próxima, é do do tipo object para aproveitar toda a classe conta.
+	 * MÃ©todo para armazenar os dados da conta e adicionar uma contagem
+	 * para a prÃ³xima, Ã© do do tipo object para aproveitar toda a classe conta.
 	 * @param nova conta
 	 */
 	public void adicionar(Object nova) {
@@ -29,14 +29,13 @@ public class Caixa {
 	}
 	
 	/**
-	 * Método que retorna os dados da conta
+	 * MÃ©todo que retorna os dados da conta
 	 * @param posicao do vetor
 	 * @return valor armazenado na posicao
 	 */
 	public Object pegar(int posicao) {
 		/*Existe um problema no getSaldo do programa
-		 *pois retorna um object e na classe objeto não existe getSaldo*/
+		 *pois retorna um object e na classe objeto nÃ£o existe getSaldo*/
 		return this.objeto[posicao];
 	}
-	
 }
