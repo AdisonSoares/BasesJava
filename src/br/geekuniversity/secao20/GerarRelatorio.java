95d14796ffb5d8748b9fd373e1630c670684f2d4
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-06-02 23:23:40 -0300

    Secao20 - Threads e syncronized

--- src/br/geekuniversity/secao20/GerarRelatorio.java
+++ src/br/geekuniversity/secao20/GerarRelatorio.java
@@ -2,16 +2,16 @@ package br.geekuniversity.secao20;
 
 public class GerarRelatorio implements Runnable{
 	public void executa() {
-		/*Laço for de 0 até 1 mil*/
+		/*LaÃ§o for de 0 atÃ© mil*/
 		for (int i = 0; i < 1000; i++) {
 			System.out.println(i+": Relatorio em andamento.....aguarde");
 		}
 	}
-	/*Depois de importar a interface runnable, para implementar a função no programa com a 
+	/*Depois de importar a interface runnable, para implementar a funÃ§Ã£o no programa com a 
 	 *thread*/	
 	@Override
 	public void run() {
-		/* Laço for de 0 até 1 mil */
+		/* LaÃ§o for de 0 atÃ© 1 mil */
 		for (int i = 0; i < 1000; i++) {
 			System.out.println(i+": Relatorio em andamento.....aguarde");
 		}
