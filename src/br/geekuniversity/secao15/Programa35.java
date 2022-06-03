660d839c9bf542ea4fedbea4efd9c3cf3e23583f
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2022-06-02 23:00:20 -0300

    Secao15 - Atributos e metodos estaticos

--- src/br/geekuniversity/secao15/Programa35.java
+++ src/br/geekuniversity/secao15/Programa35.java
@@ -1,10 +1,9 @@
-//MÈtodos Est·ticos
-/*Um mÈtodo est·tico n„o depende de uma inst‚ncia da classe
+//M√©todos Est√°ticos
+/*Um m√©todo est√°tico n√£o depende de uma inst√¢ncia da classe
 **para ser utilizado.
 **Exemplo: nomeDaClasse.metodo(){}*/
 
 package br.geekuniversity.secao15;
-
 import br.geekuniversity.secao15.Conta;
 
 @SuppressWarnings("unused")
@@ -13,10 +12,8 @@ public class Programa35 {
 	public static void main(String[] args) {
 		Conta cliente1 = new Conta("Meu cliente");
 		
-		System.out.println("N˙mero da conta: "+cliente1.getNumero());
+		System.out.println("N√∫mero da conta: "+cliente1.getNumero());
 		System.out.println("Cliente: "+cliente1.getCliente());
-		System.out.println("A prÛxima conta ser·: "+Conta.proximaConta());
-
+		System.out.println("A pr√≥xima conta ser√°: "+Conta.proximaConta());
 	}
-
 }
